dc76f54f2b8a48302bfa12fb9fe85ff376fcab95
boshtannik <boshtannik@gmail.com>
2023-05-27 13:23:56 +0300

    Forgotten embedding document - added

--- Dockerfile
+++ Dockerfile
@@ -2,5 +2,8 @@ FROM python:3.11
 RUN git clone https://github.com/boshtannik/nb_ai_assistant.git app/
 WORKDIR /app
 RUN pip install --no-cache-dir --upgrade -r requirements.txt
+RUN echo 'Parsing pdf, split it into chunks, embed it, cache it.'
+RUN python ./persist_document.py
+RUN echo 'Embedding text chunks done'
 EXPOSE 8000
 CMD ["uvicorn", "main:app", "--host", "0.0.0.0", "--port", "8000"]
---
+++ persist_document.py
@@ -0,0 +1,80 @@
+#!/usr/bin/env python3
+import os.path
+
+import pdfplumber as pdfplumber
+import PyPDF4 as PyPDF4
+from langchain.embeddings import OpenAIEmbeddings
+from langchain.vectorstores import Chroma
+
+from app_types import PageData, PDFDocument
+from utils import merge_hyphenated_words, fix_newlines, remove_multiple_newlines, clean_text, text_to_chunks, make_chain
+from config import VECTOR_STORE_COLLECTION_NAME, VECTOR_STORE_PATH
+
+
+def fill_pages_from_pdf(file_path: str, pdf_document: PDFDocument):
+    """
+    Extracts the text from each page of the PDF.
+    :param file_path: - The path of the PDF file.
+    :param pdf_document: - Python instance of document to fill.
+    """
+    if not os.path.isfile(file_path):
+        raise FileNotFoundError(f"File not found: {file_path}")
+
+    with pdfplumber.open(file_path) as pdf:
+        pdf_document.pages = []
+        for page_num, page in enumerate(pdf.pages):
+            text = page.extract_text()
+            if text.strip():  # Check if extracted text exists.
+                pdf_document.pages.append(PageData(num=page_num, text=text))
+
+
+def fill_metadata_from_pdf(file_path: str, pdf_document: PDFDocument):
+    with open(file_path, 'rb') as pdf_file:
+        reader = PyPDF4.PdfFileReader(pdf_file)
+        metadata = reader.getDocumentInfo()
+
+        pdf_document.title = metadata.get('/Title', '').strip()
+        pdf_document.author = metadata.get('/Author', '').strip()
+        pdf_document.creation_date = metadata.get('/CreationDate', '').strip()
+
+
+def parse_pdf(file_path: str) -> PDFDocument:
+    """
+    Extracts the title and text from each page of the PDF.
+
+    :param file_path: - The path of the PDF file.
+    :return: The tuple containing the title and list of tuples with page numbers
+    """
+    if not os.path.isfile(file_path):
+        raise FileNotFoundError(f"File not found: {file_path}")
+
+    pdf_document = PDFDocument('', '', '', [])
+    fill_metadata_from_pdf(file_path, pdf_document)
+    fill_pages_from_pdf(file_path, pdf_document)
+
+    return pdf_document
+
+
+if __name__ == "__main__":
+    pdf_document = parse_pdf('./Nifty Bridge Terms of Service.pdf')
+
+    pdf_document.pages = clean_text(
+        pages=pdf_document.pages,
+        cleaning_functions=[
+            merge_hyphenated_words,
+            fix_newlines,
+            remove_multiple_newlines,
+        ]
+    )
+
+    document_chunks = text_to_chunks(pdf_document)
+
+    embeddings = OpenAIEmbeddings()
+    vector_store = Chroma.from_documents(
+        document_chunks,
+        embeddings,
+        collection_name=VECTOR_STORE_COLLECTION_NAME,
+        persist_directory=VECTOR_STORE_PATH
+    )
+
+    vector_store.persist()
